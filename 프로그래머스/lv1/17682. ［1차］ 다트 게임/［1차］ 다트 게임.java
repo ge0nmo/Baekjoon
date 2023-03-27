7633dd21379a5ba460a26c6058db069c8e7e1b27
GeonMo Yang <105291117+ge0nmo@users.noreply.github.com>
2023-03-27 14:48:26 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.07 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,6 +1,55 @@
 class Solution {
     public int solution(String dartResult) {
         int answer = 0;
+        int score[] = new int[3];
+        int idx = -1;
+        
+        for(int i = 0; i < dartResult.length(); i++)
+        {
+            char x = dartResult.charAt(i);
+            if(Character.isDigit(x))
+            {
+                idx++;
+                if(x == '1' && dartResult.charAt(i + 1) == '0')
+                {
+                    score[idx] = 10;
+                    i++;
+                }
+                else
+                {
+                    score[idx] = x - '0';
+                }
+            }
+            
+            else if(x == 'S')
+                score[idx] = (int)Math.pow(score[idx], 1);
+            
+            else if(x == 'D')
+                score[idx] = (int)Math.pow(score[idx], 2);
+            
+            else if(x == 'T')
+                score[idx] = (int)Math.pow(score[idx], 3);
+            
+            else if(x == '*')
+            {
+                score[idx] *= 2;
+                if(idx > 0)
+                {
+                    score[idx - 1] *= 2;
+                }
+            }
+                
+            
+            else if(x == '#')
+                score[idx] *= -1;
+            
+        }
+        
+        for(int a : score)
+        {
+            answer += a;
+        }
+        
         return answer;
     }
 }
