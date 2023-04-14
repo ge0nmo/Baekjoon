a118111aed27cc136738d18e6ebc0f3dd897976c
GeonMo Yang <105291117+ge0nmo@users.noreply.github.com>
2023-04-14 18:11:46 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.09 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution 
+{
+    static long count = 0;
+
+    public long solution(int n) {
+        long answer = 0;
+        
+        int arr[] = new int[n + 1];
+        arr[0] = 1;
+        arr[1] = 1;
+        
+        for(int i = 2; i <= n; i++)
+        {
+            arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567;
+        }
+        
+        return arr[n];
+        
+    }
+    
+}
