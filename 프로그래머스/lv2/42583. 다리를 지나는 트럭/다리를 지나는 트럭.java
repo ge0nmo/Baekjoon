ff5ad5ba229edcd75c5d1462dd61fe61c74389ca
GeonMo Yang <105291117+ge0nmo@users.noreply.github.com>
2023-04-23 09:32:32 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.31 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        int sum = 0;
+        
+        Queue<Integer> que = new LinkedList();
+        
+        for(int i = 0; i < bridge_length; i++)
+        {
+            que.offer(0);
+        }
+        
+        que.poll();
+        que.offer(truck_weights[0]);
+        answer++;
+        sum += truck_weights[0];
+        truck_weights = Arrays.copyOfRange(truck_weights, 1, truck_weights.length);
+        
+        while(truck_weights.length != 0 || sum != 0)
+        {
+            sum -= que.poll();
+            
+            if(truck_weights.length != 0 && sum + truck_weights[0] <= weight)
+            {
+                que.offer(truck_weights[0]);
+                sum += truck_weights[0];
+                truck_weights = Arrays.copyOfRange(truck_weights, 1, truck_weights.length);
+            }
+            
+            else
+            {
+                que.offer(0);
+            }
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+}
