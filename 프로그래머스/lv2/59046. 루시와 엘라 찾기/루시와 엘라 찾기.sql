a9978cd3d60a257608da92d8811bf6d7c4ba4b9b
GeonMo Yang <mike@ticonsys.com>
2023-06-21 13:18:19 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME = 'Ella' 
+or NAME ='Lucy'
+or NAME ='Pickle'
+or NAME ='Rogan'
+or NAME ='Sabrina'
+or NAME ='Mitty'
