afd5e7fb3dabaa114b638a34dbaee35a00889f33
GeonMo Yang <ands0927@naver.com>
2023-07-18 13:50:18 +0900

    [level 2] Title: H-Index, Time: 0.49 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int length = citations.length;
+        
+        Arrays.sort(citations);
+        
+        for(int i = 0; i < length; i++)
+        {
+            int h = length - i;
+            
+            if(citations[i] >= h)
+            {
+                answer = h;
+                break;
+            }
+        }
+        
+        
+        return answer;
+    }
+    
+    public void quickSort(int left, int right, int arr[])
+    {
+        int i, j, pivot;
+        
+        if(left < right)
+        {
+            i = left;
+        j = right;
+        pivot = arr[left];
+        while(i < j)
+        {
+            while(arr[j] > pivot)
+            {
+                j--;
+            }
+            
+            while(i < j && arr[i] <= pivot)
+            {
+                i++;
+            }
+            
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+            arr[left] = i;
+            arr[i] = pivot;
+        
+            quickSort(left, i - 1, arr);
+            quickSort(i + 1, right, arr);
+        }
+        
+    }
+}
