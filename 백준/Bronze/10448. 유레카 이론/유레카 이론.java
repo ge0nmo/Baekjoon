f73ee3959f21ad2ab27cbe0142cdb2012fbbdfd0
GeonMo Yang <ands0927@naver.com>
2023-07-21 23:42:41 +0900

    [Bronze I] Title: 유레카 이론, Time: 220 ms, Memory: 13280 KB -BaekjoonHub

---
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main
+{
+    static List<Integer> list = new ArrayList();
+    
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        getTriangularNum();
+        
+        for(int i = 0; i < n; i++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            
+            if(validateNum(num))
+            {
+                System.out.println(1);
+            }
+            
+            else
+            {
+                System.out.println(0);
+            }
+        }
+    }
+    
+    public static boolean validateNum(int num)
+    {
+        for(int i = 0; i < list.size(); i++)
+        {
+            for(int j = 0; j < list.size(); j++)
+            {
+                for(int k = 0; k < list.size(); k++)
+                {
+                    int first = list.get(i);
+                    int second = list.get(j);
+                    int third = list.get(k);
+                    if(first + second + third == num)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+        
+    public static void getTriangularNum()
+    {
+        int num = 1;
+        list.add(num);
+        for(int i = 2; num <= 1000; i++)
+        {
+            num += i;
+            list.add(num);
+        }
+    }
+    
+}
