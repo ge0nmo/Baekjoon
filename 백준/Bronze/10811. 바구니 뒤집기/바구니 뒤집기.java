4f43d738e3636c9985ed064542174d175ad95332
GeonMo Yang <ands0927@naver.com>
2023-08-09 16:40:03 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 88 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    static int arr[];
+
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = i + 1;
+        }
+
+        for(int i = 0; i < m; i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            if(start != end)
+                swap(start, end);
+        }
+
+        for(int i : arr)
+        {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void swap(int start, int end)
+    {
+        int length = (end - start + 1) / 2;
+
+        for(int i = 0; i < length; i++)
+        {
+            int temp = arr[start - 1];
+            arr[start - 1] = arr[end - 1];
+            arr[end - 1] = temp;
+            end--;
+            start++;
+        }
+
+
+    }
+}
