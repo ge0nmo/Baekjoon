0c6a2cb96d0a0e0d37c4c41935db07aea87f529a
GeonMo Yang <ands0927@naver.com>
2023-08-14 16:21:29 +0900

    [unrated] Title: 덧칠하기, Time: 0.81 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,41 +1,19 @@
 class Solution {
-    
-    static int arr[];
-    
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int start = section[0];
         
-        arr = new int[n + 1];
-        
-        for(int i = 0; i < section.length; i++)
+        for(int i = 0; i < section.length;)
         {
-            int num = section[i];
-            arr[num] = 1;
-        }
-        
-        
-        for(int i = start; i < arr.length; i++)
-        {
-            if(arr[i] == 0)
-                continue;
+            int start = section[i];
+            int end = start + m - 1;
             
-            paint(i, Math.min(i + m - 1, n));
-            
+            while(i < section.length && section[i] <= end)
+            {
+                i++;
+            }
             answer++;
-            
-            i += m - 1;
         }
-        
         
         return answer;
-    }
-    
-    public void paint(int start, int end)
-    {
-        for(int i = start; i <= end; i++)
-        {
-            arr[start] = 0;
-        }
     }
 }
