78cc42a3973a203b84fb13cd3e945f7127e3c390
GeonMo Yang <ands0927@naver.com>
2024-11-14 14:39:30 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 120 ms, Memory: 16036 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -6,22 +6,16 @@ public class Main
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String arr[] = br.readLine().split(" ");
-        int a = Integer.parseInt(arr[0]);
-        int b = Integer.parseInt(arr[1]);
-        int c = Integer.parseInt(arr[2]);
-        int d = Integer.parseInt(arr[3]);
-        int e = Integer.parseInt(arr[4]);
-        int f = Integer.parseInt(arr[5]);
+        int x1 = Integer.parseInt(arr[0]);
+        int y1 = Integer.parseInt(arr[1]);
+        int c1 = Integer.parseInt(arr[2]);
+        int x2 = Integer.parseInt(arr[3]);
+        int y2 = Integer.parseInt(arr[4]);
+        int c2 = Integer.parseInt(arr[5]);
+       
+        int D = (x1 * y2) - (x2 * y1);
         
-        for(int i = -999; i <= 999; i++){
-            for(int j = -999; j <= 999; j++){
-                if((a * i) + (b * j) == c && (d * i) + (e * j) == f){
-                    System.out.print(i + " " + j);
-                    break;
-                }
-                
-            }
-        }
+        System.out.println((c1 * y2 - y1 * c2) / D + " " + (x1 * c2 - c1 * x2) / D);
         
         
     }
