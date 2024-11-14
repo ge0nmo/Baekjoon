174c4982261923ef4a57de78e82bcdb9a18f1929
GeonMo Yang <ands0927@naver.com>
2024-11-14 12:01:52 +0900

    [Bronze II] Title: 16진수, Time: 100 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/1550. 16진수/16진수.java
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -1,23 +1,9 @@
 import java.io.*;
-
 public class Main
 {
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String str = br.readLine();
-        int length = str.length();
-        int answer = 0;
-        for(int i = 0; i < length; i++)
-        {
-            char ch = str.charAt(i);
-            int n = 'A' > ch ? ch - 48 : ch - 55;
-
-            answer += Math.pow(16, length - i - 1) * n;
-        }
-
-        System.out.print(answer);
+        System.out.print(Integer.parseInt(br.readLine(), 16));
     }
-
 }
